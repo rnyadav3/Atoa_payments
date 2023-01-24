42ed9c795a688e2b8a1d0340a49b099c9bcd7a5a
Rupesh Nandan Yadav <66638693+rnyadav3@users.noreply.github.com>
2023-01-24 21:52:42 +0530

    Update dbConnection.js

--- dbConnection.js
+++ dbConnection.js
@@ -2,7 +2,7 @@ var mysql = require('mysql2');
 var conn = mysql.createConnection({
   host: 'localhost', // Replace with your host name
   user: 'root',      // Replace with your database username
-  password: 'rohitomar#12#',      // Replace with your database password
+  password: 'root',      // Replace with your database password
   database: 'node_app' // // Replace with your database Name
 }); 
  
